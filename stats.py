2bf166e8b959a25caf87bd703ceb6971810a4ae0
John Shen <shenjohn@cs.brown.edu>
2015-03-02 22:39:07 -0500

    added adjusted rand index; restructured server to accept 'partition' requests

--- stats.py
+++ stats.py
@@ -10,8 +10,12 @@ def tabulate(x_list, y_list):
 	c_table = numpy.zeros((len(x_cats), len(y_cats)))
 	for pair in zip(xinv, yinv):
 		c_table[pair[0]][pair[1]] += 1
+		
+	pretty_table = [list(y_cats)]
+	for x, row in zip(x_cats, c_table.tolist()):
+		pretty_table.append([x] + row)
 
-	return (c_table, x_cats, y_cats)
+	return (c_table, x_cats, y_cats, pretty_table)
 
 # statistics reporters
 class Fisher(object):
@@ -51,3 +55,150 @@ class Chi2(object):
 	def title(self):
 		return "Chi square test, (%s) on (%s) membership" % \
 		   (", ".join(self.x_cats), ", ".join(self.y_cats))
+
+class AdjustedRandIndex(object):
+	def __init__(self, x_labeling, y_labeling):
+		self.x_cats = list(x_labeling)
+		self.y_cats = list(y_labeling)
+
+	def calc(self, x_cluster, y_cluster):
+		# get the adjusted rand index
+		ARI = sklearn.metrics.adjusted_rand_score(x_cluster, y_cluster)
+		return {"ARI": ARI}
+
+	def title(self):
+		return "Adjusted Rand Index, (%s) vs (%s) labeling" % \
+			   (x_labeling, y_labeling)
+
+
+########## validation ########### 
+def check_same_type(lis, name):
+	t = set(map(type, lis))
+	if type(None) in t: t.remove(type(None))
+	if len(t) > 1:
+		return name + " has non-homogeneous type"
+	elif not t:
+		return name + " is all None/null"
+	return None
+
+def check_exists_and_same_type(data, field_name):
+	if field_name not in data:
+		return "Missing %s variable" % field_name
+	else:
+		return check_same_type(data[field_name], field_name)
+
+######### common data operations ##############
+	# removing nulls in pairs
+def remove_paired_nulls(raw, x_field, y_field):
+	xIndicesToRemove = set( i for i, x in enumerate(raw[x_field]) if x is None or x.lower() == 'null' )
+	yIndicesToRemove = set( i for i, y in enumerate(raw[y_field]) if y is None or y.lower() == 'null' )
+	indicesToRemove = xIndicesToRemove | yIndicesToRemove
+	raw[x_field] = [ x for i, x in enumerate(raw[x_field]) if i not in indicesToRemove ]
+	raw[y_field] = [ y for i, y in enumerate(raw[y_field]) if i not in indicesToRemove ]
+	return indicesToRemove
+	
+################ set of tests for contingency-type data ##################
+class contingency_tests(object):
+# expects that rawdata contains two homogeneous vectors under entries 'X' and 'Y' of equal length
+	def validate(self, raw): 
+		errors = []
+		for f in ['X','Y']:
+			err = check_exists_and_same_type(raw, f) 
+			if err:
+				errors.append(err)
+		
+		if 'X' in raw and 'Y' in raw: # should be same length
+			if len(raw['X']) <> len(raw['Y']):
+				errors.append('X and Y are unequal lengths')
+				
+		return errors
+
+	def tests(self, rawdata):
+		# remove the nulls
+		indicesRemoved = remove_paired_nulls(rawdata,'X','Y')
+
+		# get contingency table
+		(c_table, x_cats, y_cats, pretty_table) = \
+				  tabulate(rawdata['X'], rawdata['Y'])
+
+		# construct the results object, including the contingency table
+		result = dict(table=pretty_table, \
+					  stats={}, \
+					  samplesRemoved=len(indicesRemoved))
+
+		# check the number of categories
+		nx, ny = len(x_cats), len(y_cats)
+		if nx == 1 or ny == 1: # can't run tests on a vector
+			pass
+
+		elif nx == 2 and ny == 2:
+			# run on 2x2 table
+			fisher = Fisher(x_cats, y_cats)
+			result['stats'][fisher.title()] = fisher.calc(c_table)
+
+		else:
+			# run the r x c chi-squared test
+			chi2 = Chi2(x_cats, y_cats)
+			result['stats'][chi2.title()] = chi2.calc(c_table)
+
+			# calculate marginal distributions
+			if (nx == 2 and ny > 2) or (nx > 2 and ny == 2):
+				# calculate marginal distributions
+				margin_X = numpy.sum(c_table,1)
+				margin_Y = numpy.sum(c_table,0)
+				total = numpy.sum(margin_X)
+
+				# generate all pairs of categories
+				Xs = range(nx) if nx > 2 else [0]
+				Ys = range(ny) if ny > 2 else [0]
+				for i, j in [ (x, y) for x in Xs for y in Ys ]:
+					# Create a new dataset where we simplify the category with more
+					# than two values into a binary category
+					sub_table = numpy.zeros((2,2))
+					sub_table[0,0] = c_table[i,j]
+					sub_table[0,1] = margin_X[i] - c_table[i,j]
+					sub_table[1,0] = margin_Y[j] - c_table[i,j]
+					sub_table[1,1] = total - numpy.sum(sub_table)
+					subx_cats = [x_cats[i], "Not " + x_cats[i]] if nx > 2 else x_cats
+					suby_cats = [y_cats[j], "Not " + y_cats[j]] if ny > 2 else y_cats
+
+					# Perform Fisher's exact test
+					fisher = Fisher(subx_cats, suby_cats)
+					sub_result = fisher.calc(sub_table)
+
+					result['stats'][fisher.title()] = sub_result
+
+		return result
+
+################ set of tests for contingency-type data ##################
+class partition_tests(object):
+# expects that rawdata contains two homogeneous vectors under entries 'X' and 'Y' of equal length
+	def validate(self, raw):
+		errors = []
+		for f in ['X','Y']:
+			err = check_exists_and_same_type(raw, f) 
+			if err:
+				errors.append(err)
+		
+		if 'X' in raw and 'Y' in raw: # should be same length
+			if len(raw['X']) <> len(raw['Y']):
+				errors.append('X and Y are unequal lengths')
+
+		return errors
+
+	def tests(self, raw): # return the ARI
+		indicesRemoved = remove_paired_nulls(raw, 'X','Y')
+		result['stats'][Fisher.title()] = Fisher.calc(c_table)
+		# get the contingency table
+		(_, x_cats, y_cats, pretty_table) = \
+				  tabulate(rawdata['X'], rawdata['Y'])
+
+		ari = ARI(x_cats, y_cats)
+
+		# construct the results object, including the contingency table
+		result = dict(table=pretty_table,
+					  stats={}, \
+					  samplesRemoved=len(indicesToRemove))
+		result['stats'][ari.title()] = ari.calc()
+		
+		return result
--- statserver.py
+++ statserver.py
@@ -8,69 +8,6 @@ import tornado.ioloop
 import tornado.web
 import stats as S # local
 
-# expects that rawdata contains two homogeneous vectors under entries 'X' and 'Y' of equal length
-def contingency_tests(rawdata):
-	# remove the nulls
-	xIndicesToRemove = set( i for i, x in enumerate(rawdata['X']) if x is None or x.lower() == 'null' )
-	yIndicesToRemove = set( i for i, y in enumerate(rawdata['Y']) if y is None or y.lower() == 'null' )
-	indicesToRemove = xIndicesToRemove | yIndicesToRemove
-	rawdata['X'] = [ x for i, x in enumerate(rawdata['X']) if i not in indicesToRemove ]
-	rawdata['Y'] = [ y for i, y in enumerate(rawdata['Y']) if i not in indicesToRemove ]
-		
-	# get contingency table
-	(c_table, x_cats, y_cats) = S.tabulate(rawdata['X'], rawdata['Y'])
-
-	# construct the results object, including the contingency table
-	result = dict(table=[[""] + list(y_cats)], \
-				  stats={}, \
-				  samplesRemoved=len(indicesToRemove))
-	for x, row in zip(x_cats, c_table.tolist()):
-		result['table'].append([x] + row)
-
-	# check the number of categories
-	nx, ny = len(x_cats), len(y_cats)
-	if nx == 1 or ny == 1: # can't run tests on a vector
-		pass
-
-	elif nx == 2 and ny == 2:
-		# run on 2x2 table
-		Fisher = S.Fisher(x_cats, y_cats)
-		result['stats'][Fisher.title()] = Fisher.calc(c_table)
-
-	else:
-		# run the r x c chi-squared test
-		Chi2 = S.Chi2(x_cats, y_cats)
-		result['stats'][Chi2.title()] = Chi2.calc(c_table)
-
-		# calculate marginal distributions
-		if (nx == 2 and ny > 2) or (nx > 2 and ny == 2):
-			# calculate marginal distributions
-			margin_X = numpy.sum(c_table,1)
-			margin_Y = numpy.sum(c_table,0)
-			total = numpy.sum(margin_X)
-
-			# generate all pairs of categories
-			Xs = range(nx) if nx > 2 else [0]
-			Ys = range(ny) if ny > 2 else [0]
-			for i, j in [ (x, y) for x in Xs for y in Ys ]:
-				# Create a new dataset where we simplify the category with more
-				# than two values into a binary category
-				sub_table = numpy.zeros((2,2))
-				sub_table[0,0] = c_table[i,j]
-				sub_table[0,1] = margin_X[i] - c_table[i,j]
-				sub_table[1,0] = margin_Y[j] - c_table[i,j]
-				sub_table[1,1] = total - numpy.sum(sub_table)
-				subx_cats = [x_cats[i], "Not " + x_cats[i]] if nx > 2 else x_cats
-				suby_cats = [y_cats[j], "Not " + y_cats[j]] if ny > 2 else y_cats
-
-				# Perform Fisher's exact test
-				Fisher = S.Fisher(subx_cats, suby_cats)
-				sub_result = Fisher.calc(sub_table)
-				
-				result['stats'][Fisher.title()] = sub_result
-
-	return result
-
 # rounds all the floats in a json
 def round_all(obj, N):
 	if isinstance(obj, float):
@@ -81,39 +18,15 @@ def round_all(obj, N):
 		return map(lambda(o): round_all(o, N), obj)
 	return obj
 
+##### request handler which takes in POST requests and returns JSONs #####
 class StatsHandler(tornado.web.RequestHandler):
-	def _validate(self, raw): 
-		errors = []
-		if 'X' not in raw:
-			errors.append("Missing X variable")
-		else:
-			# check for homogeneous type
-			Xt = set(map(type, raw['X']))
-			if type(None) in Xt: Xt.remove(type(None))
-			if len(Xt) > 1:
-				errors.append("X has non-homogeneous type")
-			
-		if 'Y' not in raw:
-			errors.append("Missing Y variable")
-		else:
-			Yt = set(map(type, raw['Y']))
-			if type(None) in Yt: Yt.remove(type(None))
-			if len(Yt) > 1:
-				errors.append("Y has non-homogeneous type")
-
-		if 'X' in raw and 'Y' in raw: # should be same length
-			if len(raw['X']) <> len(raw['Y']):
-				errors.append('X and Y are unequal lengths')
-
-		return errors
-
 	# take in a dict / other jsonable object and send it back
 	def _return(self, reply):
 		# logging
 		if self.get_status() == httplib.OK:
 			table = reply['table']
 			r, c = len(table) - 1, len(table[0]) - 1
-			print "Received request (%d x %d), returning OK, categorical test results." % (r, c)
+			print "Received request type %s (%d x %d), returning OK, categorical test results." % (reply['request'], r, c)
 		elif self.get_status() == httplib.BAD_REQUEST:
 			errors = reply['Error']
 			print "Received bad request, returning BAD_REQUEST, errors: " + ";".join(errors)
@@ -132,15 +45,33 @@ class StatsHandler(tornado.web.RequestHandler):
 		# TODO: figure out why you need this, doesn't make any sense
 		if type(rawdata) == type(u""): rawdata = json.loads(rawdata)
 
+		# choose the request type
+		requestType = "contingency" # default
+		if "request" in rawdata:
+			requestType = rawdata["request"]
+
+		# check if we have that request
+		stat_packages = dict(contingency=S.contingency_tests,
+							 partitions=S.partition_tests)
+
+		if requestType not in stat_packages:
+			self.set_status(httplib.BAD_REQUEST)
+			self._return({"Error": ["Unknown request"]})
+			return
+
+		# get the statistics package
+		engine = stat_packages[requestType]()
+		
 		# check for any known errors
-		errors = self._validate(rawdata)
+		errors = engine.validate(rawdata)
 		if errors:
 			self.set_status(httplib.BAD_REQUEST)
 			self._return({"Error": errors})
 			return
 			
-		# apply result
-		result = contingency_tests(rawdata)		
+		# compute result
+		result = engine.tests(rawdata)		
+		result['request'] = requestType 
 		self._return(result)
 
 ################## server code #####################
